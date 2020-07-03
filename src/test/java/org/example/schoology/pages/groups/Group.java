4772141c352c86831f16b7dbd111066f85b93fd3
novando <ovando.noni@gmail.com>
2020-07-03 03:31:46 -0400

    fixed travis comments

--- src/test/java/org/example/schoology/pages/groups/Group.java
+++ src/test/java/org/example/schoology/pages/groups/Group.java
@@ -1,8 +1,6 @@
 package org.example.schoology.pages.groups;
 
 import org.example.core.ui.AbstractPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Group extends AbstractPage {
 
--- src/test/java/org/example/schoology/pages/groups/JoinGroupPopup.java
+++ src/test/java/org/example/schoology/pages/groups/JoinGroupPopup.java
@@ -1,9 +1,6 @@
 package org.example.schoology.pages.groups;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 public class JoinGroupPopup extends AbstractGroupPopup {
 
     public Group join(final String  accessCodeValue) {
