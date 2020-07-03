8f2c42734f538e2f121ee83679436f0c77a4972e
novando <ovando.noni@gmail.com>
2020-07-03 03:22:40 -0400

    fixed travis comments and run in a 1 runner

--- src/test/java/org/example/schoology/pages/groups/JoinGroupPopup.java
+++ src/test/java/org/example/schoology/pages/groups/JoinGroupPopup.java
@@ -1,11 +1,14 @@
 package org.example.schoology.pages.groups;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class JoinGroupPopup extends AbstractGroupPopup {
 
     public Group join(final String  accessCodeValue) {
         fillAccessCode(accessCodeValue);
-        submitButton.click();
+        action.click(submitButton);
         return new Group();
     }
 
