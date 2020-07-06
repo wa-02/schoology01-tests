ff7573d561a5fb08694941145ff7c2bd37b9e722
ALEX\Saex <saex050988@gmail.com>
2020-07-06 10:38:08 -0400

    agarcia/addTest01: Add print to debug

--- src/test/java/org/example/schoology/pages/SubMenu.java
+++ src/test/java/org/example/schoology/pages/SubMenu.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 public class SubMenu extends AbstractPage {
 
     public void clickViewListLink(final String menu) {
-        System.out.println(String.format("a[href='/%s']", menu.toLowerCase()));
         action.click(By.cssSelector(String.format("a[href='/%s']", menu.toLowerCase())));
     }
 
