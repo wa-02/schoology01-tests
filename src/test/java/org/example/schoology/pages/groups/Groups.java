5880c718fc199d7f20553ba98db24f4f318be02d
novando <ovando.noni@gmail.com>
2020-07-07 14:31:30 -0400

    Fix Wait actions in course and groups: clear import

--- src/test/java/org/example/schoology/pages/groups/Groups.java
+++ src/test/java/org/example/schoology/pages/groups/Groups.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class Groups extends ViewList {
 
