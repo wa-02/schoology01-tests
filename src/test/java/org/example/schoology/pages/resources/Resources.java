be0e634f76142a37658e6d272586b605c99d5de9
Alex Alcala Cuba <alex.alcala@jalasoft.com>
2020-07-08 00:09:34 -0700

    Share a resource Collection to a School colleague - Passed 01

--- src/test/java/org/example/schoology/pages/resources/Resources.java
+++ src/test/java/org/example/schoology/pages/resources/Resources.java
@@ -4,7 +4,6 @@ import org.example.schoology.pages.ViewList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Resources extends ViewList {
