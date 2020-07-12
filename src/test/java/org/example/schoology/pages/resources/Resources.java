4c1c3e396641bc61551964b5b20efa029bb67b84
novando <ovando.noni@gmail.com>
2020-07-12 18:37:10 -0400

    added a double wait

--- src/test/java/org/example/schoology/pages/resources/Resources.java
+++ src/test/java/org/example/schoology/pages/resources/Resources.java
@@ -30,6 +30,8 @@ public class Resources extends ViewList {
 
     public ShareSettingsPopup clickShareCollection(final String collectionName) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class=\"icon home\"]")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(String.format(COLLECTION_BY_NAME, collectionName))));
         action.click(By.xpath(String.format(COLLECTION_BY_NAME, collectionName)));
         action.click(shareButton);
         return new ShareSettingsPopup();
