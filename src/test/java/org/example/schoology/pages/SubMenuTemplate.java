f6dd01cc01e0f140eaeebff54c3145c8ea34a242
ALEX\Saex <saex050988@gmail.com>
2020-07-14 19:07:16 -0400

    agarcia/bugFixed: add implicit wait to SubMenuTemplate.java

--- src/test/java/org/example/schoology/pages/SubMenuTemplate.java
+++ src/test/java/org/example/schoology/pages/SubMenuTemplate.java
@@ -4,6 +4,8 @@ import org.example.core.ui.AbstractPage;
 import org.example.schoology.pages.resources.Resources;
 import org.openqa.selenium.By;
 
+import java.util.concurrent.TimeUnit;
+
 public class SubMenuTemplate extends AbstractPage {
 
     public static final String RESOURCE_PAGE = "//a[text()='Resources']";
@@ -14,6 +16,7 @@ public class SubMenuTemplate extends AbstractPage {
     }
 
     public SubMenu clickMenu(final String menuName) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         action.click(By.xpath(String.format("//span[text()='%s']/parent::button", menuName)));
         return new SubMenu();
     }
